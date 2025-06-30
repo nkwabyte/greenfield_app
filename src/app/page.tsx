185cc7ba4ee0d3e2e8754ad7a4ba34b585cb373e
Responsible AI Lab KNUST <responsibleailabknust@gmail.com>
2025-06-30 13:41:19 +0000

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,6 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useToast } from '@/hooks/use-toast';
-import { Logo } from '@/components/icons/logo';
-import React from 'react';
 import Image from 'next/image';
 
 export default function LoginPage() {
