3b620f4decc93225998b2fd1a2bb545888e0f565
musah <musahibrahimali@gmail.com>
2025-06-30 13:29:49 +0000

    Adding changes

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,4 +1,8 @@
-export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
+import Image from "next/image"
+
+export const Logo = (
+  props: React.SVGProps<SVGSVGElement>
+) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
